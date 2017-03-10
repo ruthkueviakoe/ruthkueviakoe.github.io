0e3f4a6c65593636203006a1312716e861f39f1c
ruthkueviakoe <ruth.1996@live.fr>
2017-03-10 15:00:47 +0100

    Update bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$.ajax({
-		url : "https://api.blockcypher.com/v1/btc/main",
+		url : "http://bitcoin.mubiz.com/blockchaininfo",
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
@@ -8,11 +8,30 @@ $(document).ready(function() {
 		async : false,
 
 		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
+			$('#blockchaininfo_blocks').append(data.blocks);
 		},
 
 		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
+			$('#blockchaininfo_blocks').append(err+" N/A");
+		}
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#info_connections').append(data.connections);
+		},
+
+		error : function(xhr, status, err) {
+			$('#info_connections').append(err+" N/A");
 		}
 	});
 });
